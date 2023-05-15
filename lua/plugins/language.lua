3b8a701aac801e7bd665aa047881795e065a06a4
donfiguerres <lyndonfiguerres@gmail.com>
2023-05-15 19:07:01 +0800

    Add renamer but not working

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -7,7 +7,17 @@ return {
   {
     "hashivim/vim-terraform",
   },
+  -- jinja
   {
     "HiPhish/jinja.vim",
+  },
+  -- LSP rename
+  {
+    "filipdutescu/renamer.nvim",
+    -- https://github.com/filipdutescu/renamer.nvim/issues/132
+    enabled = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   },
 }
